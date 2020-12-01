71b99b5d9c3f32f206fe481c5a16e2590b600215
dmdupont <72478863+dmdupont@users.noreply.github.com>
2020-12-01 13:37:04 -0800

    Update MyUnitTest.java

--- src/main/MyUnitTest.java
+++ src/main/MyUnitTest.java
@@ -8,31 +8,31 @@ public class MyUnitTest {
 	@Test
 	public void testCalculatePercentAttendance() {
 		int time = 0;
-		assertEquals(0, calculatePercentAttendance(time));
+		assertEquals(0, PlotData.calculatePercentAttendance(time));
 	}
 	
 	@Test
 	public void testCalculatePercentAttendance1() {
 		int time = 17;
-		assertEquals(25, calculatePercentAttendance(time));
+		assertEquals(25, PlotDatacalculatePercentAttendance(time));
 	}
 	
 	@Test
 	public void testCalculatePercentAttendance2() {
 		int time = 45;
-		assertEquals(60, calculatePercentAttendance(time));
+		assertEquals(60, PlotData.calculatePercentAttendance(time));
 	}
 	
 	@Test
 	public void testCalculatePercentAttendance3() {
 		int time = 59;
-		assertEquals(80, calculatePercentAttendance(time));
+		assertEquals(80, PlotData.calculatePercentAttendance(time));
 	}
 	
 	@Test
 	public void testCalculatePercentAttendance4() {
 		int time = 75;
-		assertEquals(100, calculatePercentAttendance(time));
+		assertEquals(100, PlotData.calculatePercentAttendance(time));
 	}
 	
 }
